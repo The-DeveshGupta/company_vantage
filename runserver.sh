e675a089724fe523490016d9e195d8d904ad5ade
The-DeveshGupta <devesh1997.gupta@gmail.com>
2023-03-09 23:37:24 +0530

    dockerfile added

---
+++ runserver.sh
@@ -0,0 +1,6 @@
+echo "NEW started python server"
+python3.8 manage.py makemigrations
+python3.8 manage.py migrate
+python3.8 manage.py migrate session
+python3.8 manage.py migrate
+python3.8 manage.py runserver 0.0.0.0:8000
